7191663f308964908e06a021a8dca775b5b6641f
Vladyslav Pastushok <notkiller999@gmail.com>
2025-12-29 14:43:57 +0200

    start

---
+++ backend/src/config/llm.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv"
+import { ChatOpenAI } from "@langchain/openai";
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai"
+dotenv.config()
+
+const googleApiKey = process.env.GOOGLE_API_KEY;
+const openAiApiKey = process.env.OPENAI_API_KEY;
+
+if(!googleApiKey) {
+  throw new Error("GOOGLE_API_KEY not set");
+} else if (!openAiApiKey){
+  throw new Error("OPENAI_API_KEY not set");
+}
+
+export const llmModel = () => {
+  if (googleApiKey) {
+    const googleGenAI = new ChatGoogleGenerativeAI({
+      apiKey: googleApiKey,
+      temperature: 0,
+      model: "gemini-2.5-flash",
+    });
+    return googleGenAI;
+  }
+  const openAI = new ChatOpenAI({
+    apiKey: openAiApiKey,
+    model: "gpt-4o",
+    temperature: 0,
+  })
+  return openAI
+}
