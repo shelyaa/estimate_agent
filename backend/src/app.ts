15a7e06c43aa984507d788d90f6111c0b7e45605
Aliona <ashelyaa17@gmail.com>
2025-12-29 11:43:03 +0100

    upload pdf

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 import {connectDB} from "./config/db.js";
 import {errorHandler} from "./middlewares/error.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 app.use("/api", routes);
 app.use(errorHandler)
+
 
 connectDB()
     .then(() => {
--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,42 +1,64 @@
 import type {Request, Response} from "express";
-import {getAllMessages, sendMessageToAgent} from "../services/messagesService.js"
+// import {
+//   getAllMessages,
+//   sendMessageToAgent,
+// } from "../services/messagesService.js";
+import {processPdf} from "../services/pdfService.js";
 
-export async function sendMessage(req: Request, res: Response) {
-    try {
-        const message = req?.body?.message;
-        if (!message) throw new Error("Message is required");
-        res.setHeader("Content-Type", "text/event-stream");
-        res.setHeader("Cache-Control", "no-cache");
-        res.setHeader("Connection", "keep-alive");
+// export async function sendMessage(req: Request, res: Response) {
+//   try {
+//     const message = req?.body?.message;
+//     if (!message) throw new Error("Message is required");
+//     res.setHeader("Content-Type", "text/event-stream");
+//     res.setHeader("Cache-Control", "no-cache");
+//     res.setHeader("Connection", "keep-alive");
 
-        const stream = await sendMessageToAgent(message);
+//     const stream = await sendMessageToAgent(message);
 
-        for await (const event of stream) {
-            if (event.event === "on_chat_model_stream") {
-                const token = event.data?.chunk?.content;
+//     for await (const event of stream) {
+//       if (event.event === "on_chat_model_stream") {
+//         const token = event.data?.chunk?.content;
 
-                if (token) {
-                    res.write(`data: ${token}\n\n`);
-                }
-            }
+//         if (token) {
+//           res.write(`data: ${token}\n\n`);
+//         }
+//       }
 
-            if (event.event === "on_chain_end") {
-                res.write(`event: end\ndata: done\n\n`);
-            }
-        }
+//       if (event.event === "on_chain_end") {
+//         res.write(`event: end\ndata: done\n\n`);
+//       }
+//     }
 
-        res.end();
-    } catch (err: any) {
-        res.status(500).json({ message: err.message });
+//     res.end();
+//   } catch (err: any) {
+//     res.status(500).json({message: err.message});
+//   }
+// }
+
+// export async function getMessages(req: Request, res: Response) {
+//   try {
+//     const messages = await getAllMessages();
+//     res.json(messages);
+//   } catch (err: any) {
+//     res.status(500).json({message: err.message});
+//   }
+// }
+
+export async function uploadPdf(req: Request, res: Response) {
+  try {
+    console.log(req.file)
+    if (!req.file) {
+      return res.status(400).json({message: "File not uploaded"});
     }
-}
+    console.log(req.file.path);
 
-export async function getMessages(req: Request, res: Response) {
-    try {
-        const messages = await getAllMessages();
-        res.json(messages);
-    } catch (err: any) {
-        res.status(500).json({ message: err.message });
-    }
-}
+    const docs = await processPdf(req.file.path);
 
+    res.status(200).json({
+      message: "PDF successfully uploaded and processed",
+      pageCount: docs.length,
+    });
+  } catch (err: any) {
+    res.status(500).json({error: err.message});
+  }
+}
